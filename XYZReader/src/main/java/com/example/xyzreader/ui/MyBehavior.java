fdb9503645e53b3b08d41e6bf74f40ba46169afb
Vladimir Restivo <vladimir.restivo@gmail.com>
2017-04-12 16:02:19 -0700

    refactor: got custom logo fade in behavior to work 	MyBehavior.java 	* Log the crap out of onDependentViewChanged() 	* add alpha change to logo based on AppBarLayout bottom attibute 	* add alpha value trigger based on screen scaling 	fragment_article_detail.xml 	* add tags to elements for loggin and correct indentification 	* add correct app:layout_behavior attributes 	* set logo elevation to appbar elevation 	activity_article_list.xml 	* remove on scroll title transition from main screen's toolbar, 	  and retail the app logo 	* change logo elevation

--- XYZReader/src/main/java/com/example/xyzreader/ui/MyBehavior.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/MyBehavior.java
@@ -1,9 +1,12 @@
 package com.example.xyzreader.ui;
 
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,9 +15,13 @@ import android.widget.ImageView;
  * Created by devbox on 4/10/17.
  */
 
-public class MyBehavior extends CoordinatorLayout.Behavior<Toolbar> {
+public class MyBehavior extends CoordinatorLayout.Behavior {
 
     private final String LOG_TAG = "MyBehavior";
+    private final String LOGO_TAG = "app_logo";
+    private final String PHOTO_TAG = "tag_photo";
+
+    private String mAppLogoTag;
 
     public MyBehavior() {
         super();
@@ -28,19 +35,44 @@ public class MyBehavior extends CoordinatorLayout.Behavior<Toolbar> {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, Toolbar child, View dependency) {
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        //boolean result = dependency instanceof Toolbar;
+        //boolean result = PHOTO_TAG.equals(dependency.getTag());
+
+        boolean result = dependency instanceof AppBarLayout;
+
+
+        Log.v(LOG_TAG, "_in layoutDependsOn: " + child.getTag() + " " +dependency.getTag() + " " + String.valueOf(result));
+
+
         //return super.layoutDependsOn(parent, child, dependency);
-        boolean result = dependency instanceof ImageView;
-        Log.v(LOG_TAG, "_in onDepencentViewChanged: " + String.valueOf(result));
-
-
 
         return result;
     }
 
+
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, Toolbar child, View dependency) {
-        Log.v(LOG_TAG, "_in onDepencentViewChanged: " + child.getTag());
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+
+        int bottom = dependency.getBottom();
+        DisplayMetrics dm = dependency.getContext().getResources().getDisplayMetrics();
+
+        Log.v(LOG_TAG, "_in onDepencentViewChanged: " + dependency.getTag() + " " + bottom);
+        Log.v(LOG_TAG, "_in onDepencentViewChanged: min height " + dependency.getMinimumHeight());
+        Log.v(LOG_TAG, "_in onDepencentViewChanged: rate " + dm.scaledDensity + "/" + DisplayMetrics.DENSITY_DEFAULT);
+        Log.v(LOG_TAG, "_in onDepencentViewChanged: rate " + (Math.round(dm.xdpi/DisplayMetrics.DENSITY_DEFAULT)));
+
+
+
+
+        if(bottom/dm.scaledDensity<=112) {
+            child.setAlpha(1);
+        }
+        else {
+            child.setAlpha(0);
+        }
+
+        //TODO set animation for the logo
 
         return super.onDependentViewChanged(parent, child, dependency);
     }
